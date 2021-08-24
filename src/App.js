129ca60174b682803a90b999caef84ca06984d22
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-08-24 10:20:21 +0430

    the coding in ExpensAppComponent and style it

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import ExpenseApp from "./component/ExpenseApp";
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="header">
         <h2 className="title">ExpenseTrackerApp</h2>
       </header>
-      <main>
+      <main className="main">
         <ExpenseApp />
       </main>
     </div>
--- src/component/ExpenseApp.js
+++ src/component/ExpenseApp.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const ExpenseApp = () => {
-  return <></>;
+  const [expense, setExpense] = useState(0);
+  const [income, setIncome] = useState(0);
+  return (
+    <div className="ExpenseApp">
+      <div className="sectionTop">
+        <p className="balance">Balance: {income - expense} $</p>
+        <buttton className="btn">Add</buttton>
+      </div>
+      <div className="sectionBottom">
+        <div className="expense">
+          <span className="label"> Expense: </span>
+          <span className="amountExp"> {expense} $</span>
+        </div>
+        <div className="income">
+          <span className="label"> Income: </span>
+          <span className="amountInc"> {income} $</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseApp;
